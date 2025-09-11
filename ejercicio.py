7a7b383d2ee6f2fa5e9ebd01fe4d4a432ba707ba
Fer <fmoralesflores0@gmail.com>
2025-09-11 13:51:28 -0400

    Creacion de archivo

---
+++ ejercicio.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+
+print("La suma es: ", a+b)
+
+lista = [1,"Fernando",3,4.43,5,6]
+print(lista)
+
+suma = 10
+
+if suma < 10:
+    print("El valor es menor a 10")
+else:
+    print("EL valor es mayor")
+
+#Usando un for, con un rango de 1 al 11, determinar si dicho
+#dicho numero es par o impar e imprimir cada valor
