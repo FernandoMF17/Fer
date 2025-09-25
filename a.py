b4b99a751696429030b068ff6305535eb286210a
Fernando <fmoralesflores0@gmail.com>
2025-09-25 13:13:50 -0400

    diccionarios

---
+++ a.py
@@ -0,0 +1,9 @@
+dict = {'Fernando': 15, 'Maria': 24, 'Roger': 30, 'Ana': 18}
+dict.update({'Luis': 12})
+print(dict)
+
+lista = [4,5,7,98,5,2,4,6]
+
+lista_doble = list(map(lambda x: x*2, lista))
+print(lista_doble)
+
---
+++ diccionarios.py
@@ -0,0 +1,27 @@
+lista = [1,4,5,6,4.3,"Fernando", True]
+#print(lista)
+lista[6] = "Hola"
+#print(lista)
+
+tupla = (1,4,5,6,4.3,"Fernando", True)
+#print(tupla)
+
+datos_personales = {'Fernando': '51', 'Luis' : 70, 'Cris':60, 'Ana': 20, 'Maria': 30, 'Roger': 100}
+#datos_personales['direccion'] = 'San Francisco' 
+#datos_personales.update({'correo': 'fmoralesflores0@gmnail.com'})
+print(datos_personales)
+
+def suma(a,b):
+    sum = a + b
+    return sum
+
+resultado = suma (10,5)
+print(resultado)
+
+def impares(n):
+    if (n % 2 == 1):
+        return n
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista_impares = filter(impares, lista)
+print(list(lista_impares))
